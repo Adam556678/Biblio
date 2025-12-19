1fc8c88589259f42765db0a0494452ea853ddf72
Adam556678 <mahmudramadan75@yahoo.com>
2025-12-19 02:11:24 +0200

    Category controller

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Biblio.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
