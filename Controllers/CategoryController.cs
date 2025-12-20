1108912322348619e210dd1fc46656fc2fdb119b
Adam556678 <mahmudramadan75@yahoo.com>
2025-12-20 17:41:45 +0200

    Category DB Seeding

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
+using System.Data.Common;
+using Biblio.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Biblio.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }        
     }
